dd04af55c57f2087364b239c74092c3492f62847
Liamcarvalho26 <43858635+Liamcarvalho26@users.noreply.github.com>
2024-08-29 14:58:11 +0530

    Add files via upload

---
+++ quiz game.py
@@ -0,0 +1,45 @@
+print("welcome to my computer quiz!")
+
+playing = input("do you want to play? ")
+
+if playing.lower() != "yes":
+    quit() 
+
+print("okay lets play")
+score = 0
+
+answer = input("what does cpu  stand for? ").lower()
+if answer == "central processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect") 
+
+answer = input("what does gpu stand for? ").lower
+if answer == "graphics processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect") 
+
+answer = input("what does ram stand for? ").lower()
+if answer == "random access memory":
+    print("correct!")
+    score += 1 
+else:
+    print("incorrect") 
+
+answer = input("what does psu stand for? ").lower()
+if answer == "power supply":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect") 
+
+print("your score is " + str(score)+ " questions correct")
+print("you got " + str((score/4) *100) + " percent")
+
+
+ 
+
+
