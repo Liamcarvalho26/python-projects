c9b9f13221b42708b30102a2a5bd63a9f78fa0d0
Liamcarvalho26 <43858635+Liamcarvalho26@users.noreply.github.com>
2024-08-31 19:53:11 +0530

    Add files via upload

---
+++ bot/learning.py
@@ -0,0 +1,15 @@
+
+##numbers = list(range(20))
+##print(numbers)
+
+"""chars = list("hello world")
+print(chars)
+print(len(chars))"""
+
+'''letters = list("abcd")
+print(letters)
+print(letters[-4])'''
+
+letters = ["a","b","c"]
+for i in enumerate(letters):
+    print(i)
